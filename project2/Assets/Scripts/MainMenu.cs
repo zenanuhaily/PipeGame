b12eb740f9e2f5aef26d356eeb7707bff5e09aa7
zenanuhaily <nuhai003@cougars.csusm.edu>
2018-03-27 23:47:26 -0700

    first commit

---
+++ project2/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MainMenu : MonoBehaviour {
+
+	public Player player;
+
+	public Text scoreLabel;
+
+    public Text HowToPlay;
+
+	private void Awake () {
+		Application.targetFrameRate = 1000;
+	}
+
+	public void StartGame (int mode) {
+		player.StartGame(mode);
+		gameObject.SetActive(false);
+		Cursor.visible = false;
+        
+    }
+
+	public void EndGame (float distanceTraveled) {
+		scoreLabel.text = ((int)(distanceTraveled * 10f)).ToString();
+		gameObject.SetActive(true);
+		Cursor.visible = true;
+        
+	}
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
